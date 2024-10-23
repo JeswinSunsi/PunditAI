c51734473c0129762cd1ec6339acef53718b555d
Lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2024-10-23 08:34:43 +0400

    Add hero, navbar

---
+++ src/components/hero.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="main">
+    <h1 class="hero-heading">Answers that go the distance</h1>
+    <h2 class="hero-subheading">Generate 10,000+ words from a single prompt, designed for thinkers & creators.
+    </h2>
+    <div class="flex-wrap-2">
+      <div class="btn">
+        <h1 class="btn-1">Try for free</h1>
+      </div>
+      <div class="btn">
+        <h1 class="btn-2">View Pricing</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.main {
+  padding-top: 15vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: Poppins;
+}
+
+.hero-heading {
+  font-size: 3.5rem;
+  font-weight: 800;
+  color: #130540;
+  text-align: center;
+  line-height: 6rem;
+}
+
+.hero-subheading {
+  font-size: 1.2rem;
+  color: rgba(19, 5, 64, 0.8);
+  text-align: center;
+  width: 40vw;
+  line-height: 1.5rem;
+}
+
+.flex-wrap-2 {
+  display: flex;
+}
+
+.flex-wrap-2 div {
+  margin-top: 5vh;
+  padding: 0.6rem 1.3rem;
+  cursor: pointer;
+  margin-right: 0.6rem;
+  border-radius: 0.3rem;
+  background-color: #130540;
+  color: #fff;
+}
+
+.flex-wrap-2 div:first-child {
+  color: #130540;
+  border: solid 1px #130540;
+  background-color: #fff;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="main">
+        <img src="../assets/logo.png" alt="Pundit AI">
+        <div class="flex-wrap-1">
+            <div class="item">
+                <h1>Product</h1>
+                <img src="../assets/southArrow.png" alt="Navigate to Link">
+            </div>
+            <div class="item">
+                <h1>Features</h1>
+                <img src="../assets/southArrow.png" alt="Navigate to Link">
+            </div>
+            <div class="item">
+                <h1>Pricing</h1>
+                <img src="../assets/southArrow.png" alt="Navigate to Link">
+            </div>
+            <div class="item">
+                <h1>Github</h1>
+            </div>
+        </div>
+        <div class="flex-wrap-2">
+            <div class="btn">
+                <h1 class="btn-1">Login</h1>
+            </div>
+            <div class="btn">
+                <h1 class="btn-2">Try for free</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+img {
+    cursor: pointer;
+    height: 2rem;
+}
+
+.main {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 4rem 0.5rem 4rem;
+    background-color: #fff;
+    font-family: Poppins;
+    font-weight: 400;
+    color: #130540;
+    position: sticky;
+    z-index: 100;
+    top: 0;
+}
+
+.flex-wrap-1 {
+    display: flex;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+}
+
+.item {
+    padding: 0rem 2rem;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.item img {
+    margin-left: 0.5rem;
+    height: 1rem;
+}
+
+.flex-wrap-2 {
+    display: flex;
+}
+
+.flex-wrap-2 div {
+    padding: 0.6rem 1.3rem;
+    cursor: pointer;
+    margin-right: 0.6rem;
+    border-radius: 0.3rem;
+    background-color: #130540;
+    color: #fff;
+}
+
+.flex-wrap-2 div:first-child {
+    color: #130540;
+    border: solid 1px #130540;
+    background-color: #fff;
+}
+
+@media only screen and (max-width: 600px) {
+    .flex-wrap-2 div:first-child {}
+
+    .flex-wrap-1 {
+        display: none;
+    }
+
+    .main {
+        padding: 1rem;
+    }
+}
+</style>
