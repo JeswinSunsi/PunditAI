5893119d331b43f4ee9611e6af91b0c89d3756cb
Lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2024-10-23 08:35:08 +0400

    Create footer

---
+++ src/components/footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="main">
+        Made with ☕ & 💖 by <a href="https://github.com/jeswinsunsi" target="_blank">Jezwin Sunsi</a>&<a
+            href="https://github.com/Aashin20" target="_blank">Aashin Anil</a>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.main {
+    margin-top: 3rem;
+    width: 100%;
+    height: 7rem;
+    background-color: #FFF;
+    border-top: solid 1px rgba(19, 5, 64, 0.8);
+    font-family: Poppins;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+a {
+    color: #42CF71 !important;
+    text-decoration: underline;
+    color: rgb(19, 5, 64);
+    cursor: pointer;
+    font-weight: 600;
+    margin-right: 0.3rem;
+    margin-left: 0.3rem;
+}
+</style>
